4b9c448c02f1de171cd4d65141aca17b9657f46d
aglazev <andreiglazev@mail.ru>
2022-11-08 20:29:23 +0300

    log

---
+++ BartMarket/Controllers/LogController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+
+namespace BartMarket.Controllers
+{
+    [ApiController]
+    [Route("api/log")]
+    [Authorize]
+    public class LogController : Controller
+    {
+        public string Index()
+        {
+            using (StreamReader file = new StreamReader(AppDomain.CurrentDomain.BaseDirectory + "/logs/debug.log"))
+            {
+                return file.ReadToEnd();
+            }
+        }
+    }
+}
--- BartMarket/Program.cs
+++ BartMarket/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog;
+using Quartz.Logging;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -9,10 +11,13 @@ using System.Diagnostics;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Serialization;
+using Logger = NLog.Logger;
 
 namespace BartMarket
 {
@@ -21,6 +26,7 @@ namespace BartMarket
         public static string formula1 = "(x + 1500) * 1.2";
         public static string formula2 = "(x + 1500) * 1.5";
         public static string formula3 = "(x + 1500) * 1.15";
+        private static Logger logger = LogManager.GetCurrentClassLogger();
 
         public static void Main(string[] args)
         {
@@ -75,13 +81,43 @@ namespace BartMarket
         {
             XmlSerializer serializer = new XmlSerializer(typeof(YmlCatalog));
             YmlCatalog catalog = new YmlCatalog();
+
             var text = File.ReadAllText("Example1.xml");
             using (StringReader reader = new StringReader(text))
             {
                 var text2 = serializer.Deserialize(reader);
                 catalog = (YmlCatalog)text2;
             }
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    using (var s = client.GetStreamAsync("https://partners.donplafon.ru/local/partners/BARTMARKET_XML_CONTENT/"))
+                    {
+                        using (var fs = new FileStream("exmp2.xml", FileMode.OpenOrCreate))
+                        {
+                            s.Result.CopyTo(fs);
+                            logger.Info("success");
 
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex.Message);
+            }
+       
+            if (File.Exists("exmp2"))
+            {
+                logger.Info(File.ReadAllText("exmp2.xml"));
+
+            }
+            else
+            {
+                logger.Error("No acceess");
+            }
 
             YmlCatalog catalog2 = new YmlCatalog();
             var text22 = File.ReadAllText("Example22.xml");
