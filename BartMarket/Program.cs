50b36a09f22c45e24910ae43487f2805ad2eb42e
aglazev <andreiglazev@mail.ru>
2022-11-10 17:35:18 +0300

    r

--- BartMarket/Program.cs
+++ BartMarket/Program.cs
@@ -46,6 +46,23 @@ namespace BartMarket
             using (var db = new UserContext())
             {
                 var l = db.LinkModels.ToList();
+                if(l.Count == 0)
+                {
+                    var lite = new LinkModel();
+                    lite.Link = link_ozon_lite;
+                    lite.Type = "Lite";
+
+                    var lite2 = new LinkModel();
+                    lite2.Link = link_ozon_full;
+                    lite2.Type = "Full";
+
+                    db.LinkModels.Add(lite);
+                    db.LinkModels.Add(lite2);
+                    db.SaveChanges();
+
+
+                }
+
                 link_ozon_full = l.FirstOrDefault(m=>m.Type == "Full").Link;
                 link_ozon_lite = l.FirstOrDefault(m => m.Type == "Lite").Link;
 
@@ -57,6 +74,44 @@ namespace BartMarket
                 n.Name = item.Name;
                 using (var db = new UserContext())
                 {
+                    var houses = db.Warehouses.ToList();
+
+                    if(houses.Count == 0)
+                    {
+                        var _1 = new WarehouseModel();
+                        var _2 = new WarehouseModel();
+                        var _3 = new WarehouseModel();
+
+                        _1.Name = "DPN";
+                        _2.Name = "DPN2";
+                        _3.Name = "DPN3";
+
+                        db.Warehouses.Add(_1);
+                        db.Warehouses.Add(_2);
+                        db.Warehouses.Add(_3);
+
+                        var sett = new List<WarehouseSetting>();
+
+                        var sett1 = new WarehouseSetting();
+                        sett1.WarehouseId = 2;
+                        sett1.Filter = "price > 3000";
+
+                        var sett2 = new WarehouseSetting();
+                        sett2.WarehouseId = 2;
+                        sett2.Filter = "price < 50000";
+
+                        var sett3 = new WarehouseSetting();
+                        sett3.WarehouseId = 2;
+                        sett3.Filter = "weight < 30";
+
+                        db.WarehouseSettings.AddRange(sett1,sett2, sett3);
+
+                        db.SaveChanges();
+                        break;
+                    }
+
+
+
                     var set = db.WarehouseSettings.Where(m=>m.WarehouseId == item.Id).ToList();
                     if(set != null)
                     {
