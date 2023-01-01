7b5cbb6bba9f324ae6bc8cf17314ebeb7d6aff92
aglazev <andreiglazev@mail.ru>
2023-01-01 16:28:47 +0300

    авпвап

--- BartMarket/Data/UserContext.cs
+++ BartMarket/Data/UserContext.cs
@@ -16,6 +16,9 @@ namespace BartMarket.Data
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite("Filename=Data.db");
+            optionsBuilder.UseMySql("server=localhost;user=root;password=448agiAoi;database=mysql;");
+            //  optionsBuilder.UseMySql("server=n50jp.spectrum.myjino.ru;user=1231402_wp1;password=WDD$,p|h3/r8e;database=1231402_wp1;");
+            //optionsBuilder.UseSqlite("Filename=Data.db");
         }
     }
 }
