0b2a62d169708d04cbf02ad4d97e34c208e08a8a
aglazev <andreiglazev@mail.ru>
2023-01-07 13:40:03 +0300

    d

--- BartMarket/Template/NapolnyTorsher.cs
+++ BartMarket/Template/NapolnyTorsher.cs
@@ -38,28 +38,28 @@ namespace BartMarket.Template
             {
                 used = db.UploadedOzonIds.ToList();
             }
-            try
-            {
-                var text = File.ReadAllText("wwwroot" + Program.link_ozon_full);
+            //try
+            //{
+            //    var text = File.ReadAllText("wwwroot" + Program.link_ozon_full);
 
 
-                XmlSerializer serializer2 = new XmlSerializer(typeof(YmlCatalog2));
-                YmlCatalog2 catalog3 = new YmlCatalog2();
+            //    XmlSerializer serializer2 = new XmlSerializer(typeof(YmlCatalog2));
+            //    YmlCatalog2 catalog3 = new YmlCatalog2();
 
-                using (StringReader reader = new StringReader(text))
-                {
-                    var text2 = serializer2.Deserialize(reader);
-                    catalog3 = (YmlCatalog2)text2;
-                }
+            //    using (StringReader reader = new StringReader(text))
+            //    {
+            //        var text2 = serializer2.Deserialize(reader);
+            //        catalog3 = (YmlCatalog2)text2;
+            //    }
 
-                logger.Info($"zaro list.count " + catalog3.Shop.Offers.Offer);
+            //    logger.Info($"zaro list.count " + catalog3.Shop.Offers.Offer);
 
-                Program.list = catalog3.Shop.Offers.Offer;
-            }
-            catch (Exception ex5)
-            {
-                logger.Error(ex5.Message);
-            }
+            //    Program.list = catalog3.Shop.Offers.Offer;
+            //}
+            //catch (Exception ex5)
+            //{
+            //    logger.Error(ex5.Message);
+            //}
 
             var list = new List<Offer2>();
 
