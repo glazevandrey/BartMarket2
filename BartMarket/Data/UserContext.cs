3e60e14ed3171db5f571ed97ec60502a7a5d48f6
aglazev <andreiglazev@mail.ru>
2023-01-08 14:26:39 +0300

    ва

--- BartMarket/Data/UserContext.cs
+++ BartMarket/Data/UserContext.cs
@@ -17,7 +17,7 @@ namespace BartMarket.Data
         {
             //  optionsBuilder.UseMySql(@"server=mysql.j48317898.myjino.ru;user=j48317898;password=448agiAoi;database=j48317898;");
             // optionsBuilder.UseSqlite("Filename=Data.db");
-            optionsBuilder.UseMySql("server=sql7.freesqldatabase.com;user=sql7587596;password=HeuM2vpkl1;database=sql7587596;");
+            optionsBuilder.UseMySql("server=db4free.net;user=bartmarket;password=448agiAoi;database=bartmarket;");
             //  optionsBuilder.UseMySql("server=n50jp.spectrum.myjino.ru;user=1231402_wp1;password=WDD$,p|h3/r8e;database=1231402_wp1;");
             //optionsBuilder.UseSqlite("Filename=Data.db");
         }
